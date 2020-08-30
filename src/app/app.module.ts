0529227e7bf9964e8dfa3286426c467afb9ef10a
mohaned benmansour <mohaned.benmansour@esprit.tn>
2020-08-30 11:22:42 +0100

    setting up the envirement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
+import { MerchantPageComponent } from './components/merchant-page/merchant-page.component';
+import { ClientPageComponent } from './components/client-page/client-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    AdminPageComponent,
+    MerchantPageComponent,
+    ClientPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/merchant-page/merchant-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-merchant-page',
+  templateUrl: './merchant-page.component.html',
+  styleUrls: ['./merchant-page.component.scss']
+})
+export class MerchantPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
